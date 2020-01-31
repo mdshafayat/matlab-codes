3b5f3a164a0157ec47f9af597d569aaa7e6bc3f5
Md Shafayat Hossain <52263071+mdshafayat@users.noreply.github.com>
2020-01-31 14:41:24 -0500

    Update CFcommensurability.m

--- CFcommensurability.m
+++ CFcommensurability.m
@@ -4,13 +4,13 @@ close all;
 a=190e-9;
 h=6.63e-34;
 hbar=h/2/pi;
-% n=1.452e15;
+% n=1.452e15; %2DES density
 
-Bhalf=2.7411;
+Bhalf=2.7411; % location of nu=1/2
 n=1/2*Bhalf*2.414*1e14
 e=1.6e-19/1;
 i=1;
-% Rc= [1 1.74 2.74].*a/2
+% Rc= [1 1.74 2.74].*a/2  %triangular lattice
 Rc=(i+1/4).*a/2;
 Kf=sqrt(4*pi*n)
 Bstar=hbar*Kf./Rc/e;
@@ -20,17 +20,6 @@ gg1=e/2/h;
 Bi = 0.0323*7;
 Kf = Bi*e*a*(1+1/4)/2/hbar;
 
-COposCF=Bstar+Bhalf;
-COposCF-Bhalf
+COposCF=Bstar+Bhalf; % Geometric resonance minima on nu<1/2 side
 
-COposCF=Bstar*sqrt(COposCF/Bhalf)+Bhalf;
-COposCF=Bstar*sqrt(COposCF/Bhalf)+Bhalf;
-COposCF=Bstar*sqrt(COposCF/Bhalf)+Bhalf;
-COposCF=1/2*(Bstar+1/2*Bstar.*Bstar/Bhalf)+Bhalf;
-
-COposCF=-Bstar+Bhalf;
-COposCF=-Bstar*sqrt(COposCF/Bhalf)+Bhalf;
-COposCF=-Bstar*sqrt(COposCF/Bhalf)+Bhalf;
-COposCF=-Bstar*sqrt(COposCF/Bhalf)+Bhalf;
-COposCF=(-Bstar+Bstar.*Bstar/Bhalf)+Bhalf;
-COposCF-Bhalf
+COposCF=(-Bstar+Bstar.*Bstar/Bhalf)+Bhalf;  % Geometric resonance minima on nu>1/2 side
